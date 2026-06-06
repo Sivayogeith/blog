4af415b1378f7e8d8e7302083c6e0f505dae8b88
Sivayogeith Umamaheswaran <sivayogeith@gmail.com>
2026-06-06 12:50:26 +0530

    feat: added a basic home page listing blogs from the api

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,46 @@
 import Image from "next/image";
+import { remark } from "remark";
+import html from "remark-html";
 
-export default function Home() {
+const getPosts = async () => {
+  let data = await fetch(`${process.env.API}/posts`);
+  let posts: { id: number; title: string; body: string }[] = await data.json();
+  posts.map(async (post) => {
+    const bodyMD = await remark().use(html).process(post.body);
+    const bodyHTML = bodyMD.toString();
+    console.log(bodyHTML);
+    post.body = bodyHTML;
+  });
+
+  return posts
+}
+export default async function Home() {
+  const posts = await getPosts();
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+    <div className="flex flex-col flex-1 items-center bg-zinc-50 dark:bg-[#000217] py-10">
+      <h1 className="text-5xl mb-5">Sage's Blog</h1>
+      {posts.map(async (post) => (
+        <div
+          key={post.id}
+          className="p-10 my-5 w-2/3 border rounded-2xl flex justify-between"
+        >
+          <div>
+            <h2 className="text-3xl">{post.title}</h2>
+            <div
+              className="text-xl"
+              dangerouslySetInnerHTML={{ __html: post.body }}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
+          <Image
+            src="/cats.png"
+            alt="cats"
+            width={500}
+            height={500}
+            className="rounded-xl"
+          />
         </div>
-      </main>
+      ))}
     </div>
   );
 }
