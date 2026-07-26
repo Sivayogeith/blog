052930106e76ebe900e04fa5cb18bde83ee4038d
Sivayogeith Umamaheswaran <sivayogeith@gmail.com>
2026-07-26 12:46:06 +0530

    feat: added post and comments stats to /user page
    added getUserStats API function too

--- src/api/userAPI.ts
+++ src/api/userAPI.ts
@@ -6,3 +6,8 @@ export const getUser = async (username: string): Promise<User> => {
     const response = await get(`/user/${username}`)
     return response.ok ? await response.json() : {} as User
 }
+
+export const getUserStats = async (username: string): Promise<{posts: number, comments: number}> => {
+    const response = await get(`/user/${username}/stats`)
+    return response.ok ? response.json() : {} as ReturnType<typeof getUserStats>
+}
--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -1,5 +1,5 @@
-import { getUser } from "@/src/api/userAPI";
-import Image from "next/image"
+import { getUser, getUserStats } from "@/src/api/userAPI";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
 export default async function UserPage({
@@ -7,19 +7,40 @@ export default async function UserPage({
 }: {
   params: Promise<{ username: string }>;
 }) {
-    const { username } = await params
-    const user = await getUser(username)
-    if (!user.name) {
-      return notFound()
-    }
+  const { username } = await params;
+  const user = await getUser(username);
+  const stats = await getUserStats(username);
+  if (!user.name) {
+    return notFound();
+  }
 
-    return <>
-        <div className="flex py-4 px-20 gap-5">
-            <Image src={user.image || "/default-user.png"} alt={`${user.name}'s Profile Picture`} width={128} height={128} unoptimized className="rounded-full border dark:border-lighter border-dark"/>
-            <div className="pt-2">
-              <h1 className="text-5xl">{user.name}</h1>
-              <p className="text-xl opacity-75">@{user.username}</p>
-            </div>
+  return (
+    <>
+      <div className="flex py-4 ps-20 gap-5">
+        <Image
+          src={user.image || "/default-user.png"}
+          alt={`${user.name}'s Profile Picture`}
+          width={128}
+          height={128}
+          unoptimized
+          className="rounded-full border dark:border-lighter border-dark"
+        />
+        <div className="pt-2">
+          <h1 className="text-5xl">{user.name}</h1>
+          <p className="text-xl opacity-75">@{user.username}</p>
         </div>
+        <div className="w-[50%]"></div>
+        <div className="flex gap-5 items-center justify-between w-[25%] pe-5 pt-2">
+          <div>
+            <p>Posts Authored</p>
+            <p className="text-5xl mt-1 text-center">{stats.posts}</p>
+          </div>
+          <div>
+            <p>Comments Made</p>
+            <p className="text-5xl mt-1 text-center">{stats.comments}</p>
+          </div>
+        </div>
+      </div>
     </>
+  );
 }
