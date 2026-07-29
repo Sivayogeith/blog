7e2aba10bebb1a48e6bce91593b6cf7dcbf8c39e
Sivayogeith Umamaheswaran <sivayogeith@gmail.com>
2026-07-29 20:26:09 +0530

    feat: added time ago to comments

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,3 +1,4 @@
+import { formatDistance } from "date-fns";
 import { Post } from "../api/helper";
 import { getComments } from "../api/postsAPI";
 import CommentForm from "./CommentForm";
@@ -7,15 +8,18 @@ export default async function Comments({ post }: { post: Post }) {
 
   return (
     <>
-      <CommentForm post={post}/>
+      <CommentForm post={post} />
       <p className="text-xl font-bold mt-4">Comments</p>
-      <div className="mt-1">
+      <div className="mt-1 ms-3">
         {comments.map((comment) => (
           <div className="mb-4" key={comment.id}>
-            <a className="text-lg font-bold" href={`/user/${comment.from}`}>
-              @{comment.from}
-            </a>
-            <p className="text-lg">{comment.message}</p>
+            <div className="flex gap-1 items-center">
+              <a className="text-lg font-bold" href={`/user/${comment.from}`}>
+                @{comment.from}
+              </a>
+              <span className="text-sm">• {formatDistance(comment.created_at, Date.now(), { addSuffix: true })}</span>
+            </div>
+            <p className="text-lg ms-2">{comment.message}</p>
           </div>
         ))}
       </div>
