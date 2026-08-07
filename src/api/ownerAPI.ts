a98f18cd869d0eff44a247569fa47281676165b8
Sivayogeith Umamaheswaran <sivayogeith@gmail.com>
2026-08-07 14:53:38 +0530

    feat: added getUsers API func

--- src/api/ownerAPI.ts
+++ src/api/ownerAPI.ts
@@ -1,11 +1,16 @@
-import { del, post } from "./helper"
+import { del, post, get } from "./helper";
+
+export const getUsers = async (username: string) => {
+  const response = await get("/owner/getUsers");
+  return response.json()
+};
 
 export const addAdmin = async (username: string) => {
-    const response = await post("/owner/addAdmin", { username })
-    return { message: await response.text(), status: response.status }
-} 
+  const response = await post("/owner/addAdmin", { username });
+  return { message: await response.text(), status: response.status };
+};
 
 export const removeAdmin = async (username: string) => {
-    const response = await del("/owner/removeAdmin", { username })
-    return { message: await response.text(), status: response.status }
-}
+  const response = await del("/owner/removeAdmin", { username });
+  return { message: await response.text(), status: response.status };
+};
