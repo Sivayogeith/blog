61519258d7c00eb293bb8d532d1fac4de035800c
Sivayogeith Umamaheswaran <sivayogeith@gmail.com>
2026-08-07 20:24:38 +0530

    feat: changes to users table, see desc
    added title of table ("All Users"), "you" to the user who is the currently logged in user and changed true/false to Yes/No in Admin? and Owner?

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,33 +1,44 @@
+import { getMe } from "@/src/api/authAPI";
 import { getUsers } from "@/src/api/ownerAPI";
 
 export default async function Users() {
   const users = await getUsers();
+  const session = await getMe();
+  
   return (
-    <div className="p-10"> 
-      <table className="table-auto w-full">
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Username</th>
-            <th>Image</th>
-            <th>Admin?</th>
-            <th>Owner?</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>#{user.id}</td>
-              <td>{user.name}</td>
-              <td>{user.username}</td>
-              <td className="flex justify-center"><img className="size-10 rounded-full" src={user.image} /></td>
-              <td>{user.isAdmin.toString()}</td>
-              <td>{user.isOwner.toString()}</td>
+    <div className="flex flex-col items-center w-full">
+      <div className="m-5 md:w-[95vw] w-[90vw] h-full flex justify-between flex-col">
+        <p className="text-2xl">All Users</p>
+        <hr className="dark:text-lightest text-dark" />
+      </div>
+      <div className="px-10 w-full">
+        <table className="table-auto w-full">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Username</th>
+              <th>Image</th>
+              <th>Admin?</th>
+              <th>Owner?</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.id}>
+                <td>#{user.id}</td>
+                <td>{user.name} <span className="font-semibold">{user.username == session.username && "(you)"}</span></td>
+                <td>{user.username}</td>
+                <td className="flex justify-center">
+                  <img className="size-10 rounded-full" src={user.image} />
+                </td>
+                <td>{user.isAdmin ? "Yes" : "No"}</td>
+                <td>{user.isOwner ? "Yes" : "No"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
