3122c2a824fb5cf49616f93c586e413243e5a009
Sivayogeith Umamaheswaran <sivayogeith@gmail.com>
2026-08-21 16:48:39 +0530

    feat: added functional comments editing

--- src/components/CommentClient.tsx
+++ src/components/CommentClient.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import { formatDistance } from "date-fns";
-import { Comment, deleteComment } from "../api/commentsAPI";
+import { Comment, deleteComment, editComment } from "../api/commentsAPI";
 import { SessionData } from "../api/authAPI";
 import DeleteIcon from "./icons/DeleteIcon";
 import EditIcon from "./icons/EditIcon";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { commentSchema } from "./CommentForm";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function CommentClient({
   comments,
@@ -15,14 +19,34 @@ export default function CommentClient({
   comments: Comment[];
   session: SessionData;
 }) {
-  const router = useRouter()
+  const router = useRouter();
+  const [edit, setEdit] = useState<number | null>(null);
+  const { register, getValues, setValue } = useForm({
+    resolver: zodResolver(commentSchema),
+    mode: "onTouched",
+  });
+
+  const onEdit = async (commentId: number) => {
+    const message = getValues("text");
+    console.log(message);
+    toast.promise(editComment(commentId, message), {
+      loading: "Editing...",
+      success: ({ message, status }) => {
+        setEdit(null)
+        if (status == 200) {
+          router.refresh();
+        }
+        return { type: status == 200 ? "success" : "error", message };
+      },
+    });
+  };
 
   const onDelete = async (commentId: number) => {
     toast.promise(deleteComment(commentId), {
       loading: "Deleting comment...",
       success: ({ message, status }) => {
         if (status == 200) {
-            router.refresh()
+          router.refresh();
         }
         return { type: status == 200 ? "success" : "error", message };
       },
@@ -39,7 +63,7 @@ export default function CommentClient({
                 src={comment.image}
                 className="size-7 rounded-full me-1 mt-0.5"
               />
-              <div className="w-full">
+              <form className="w-full">
                 <div className="flex items-center justify-between">
                   <div className="flex gap-1 items-center">
                     <a
@@ -56,16 +80,44 @@ export default function CommentClient({
                     </span>
                   </div>
                 </div>
-                <p className="text-lg ms-1">{comment.message}</p>
-              </div>
+                {edit == comment.id ? (
+                  <div className="w-[98%] flex flex-col mt-1">
+                    <textarea {...register("text")} />
+                    <button
+                      className="border-0! p-0! text-end mt-1"
+                      onClick={() => onEdit(comment.id)}
+                      type="button"
+                    >
+                      save
+                    </button>
+                  </div>
+                ) : (
+                  <p className="text-lg ms-1">{comment.message}</p>
+                )}
+              </form>
               {comment.from == session.username && (
                 <div className="flex flex-col gap-4 items-center h-full">
-                  <button className="border-0! p-0!">
-                    <EditIcon className="size-4.5" />
-                  </button>
-                  <button className="border-0! p-0!" onClick={() => onDelete(comment.id)}>
-                    <DeleteIcon className="size-5 dark:text-red-400 text-red-700" />
-                  </button>
+                  {edit !== comment.id && (
+                    <>
+                      <button
+                        className="border-0! p-0!"
+                        onClick={() => {
+                          setEdit(comment.id);
+                          setValue("text", comment.message);
+                        }}
+                        type="button"
+                      >
+                        <EditIcon className="size-4.5" />
+                      </button>
+                      <button
+                        className="border-0! p-0!"
+                        onClick={() => onDelete(comment.id)}
+                        type="button"
+                      >
+                        <DeleteIcon className="size-5 dark:text-red-400 text-red-700" />
+                      </button>
+                    </>
+                  )}
                 </div>
               )}
             </div>
